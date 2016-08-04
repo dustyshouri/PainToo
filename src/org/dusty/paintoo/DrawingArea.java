04af47c25959bdc7f6f89aa7ae1de8528657c90f
Deas <neo_graal@hotmail.com>
2016-08-04 20:31:28 +0200

    Orginized Imports and SerialUID

--- src/org/dusty/paintoo/DrawingArea.java
+++ src/org/dusty/paintoo/DrawingArea.java
@@ -1,31 +1,12 @@
 package org.dusty.paintoo;
-import java.awt.AlphaComposite;
-import java.awt.Color;
-import java.awt.Composite;
-import java.awt.Cursor;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Point;
-import java.awt.RenderingHints;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.image.BufferedImage;
-
-import javax.swing.JComponent;
-
-import org.dusty.paintoo.PainToo.Tool;
-import org.dusty.paintoo.PaintCursor.Cursors;
 
 public class DrawingArea extends Layer {
-  
+  private static final long serialVersionUID = 1L;
+
   public DrawingArea(PainToo paint) {
     super(paint);
     this.isDrawingArea = true;
-
-    
+   
     super.setPreferredSize(dimension);
   }
  
